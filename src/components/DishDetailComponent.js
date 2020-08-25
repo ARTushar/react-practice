4cc4fcead336e557c7839d6576090911d16a1f1f
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-08-25 19:14:54 +0600

    combine reducers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form';
 
 
-function RenderDish({dish}){
+function RenderDish({ dish }) {
     return (
         <Card>
             <CardImg top src={dish.image} alt={dish.name} />
@@ -19,23 +20,23 @@ function RenderDish({dish}){
     );
 }
 
-function formatDate(dateStr){
+function formatDate(dateStr) {
     let date = new Date(dateStr)
-    let options = {year: 'numeric', month: 'short', day: 'numeric'}
+    let options = { year: 'numeric', month: 'short', day: 'numeric' }
     return date.toLocaleDateString("en-US", options)
 }
 
-function RenderComments({comments}){
-    if (comments != null){
-        const comment = comments.map((comment) =>{
+function RenderComments({ comments }) {
+    if (comments != null) {
+        const comment = comments.map((comment) => {
             // let date = this.formatDate(comment.date);
             return (
-            <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>
-                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                </p>
-            </li>
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>
+                        -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                    </p>
+                </li>
             );
         });
         return (
@@ -44,6 +45,7 @@ function RenderComments({comments}){
                 <ul className="list-unstyled">
                     {comment}
                 </ul>
+                <CommentForm />
             </div>
         );
     } else {
@@ -76,8 +78,91 @@ const DishDetail = (props) => {
             </div>
 
         );
-    } else{
+    } else {
         return <div></div>
+    }
+}
+
+const required = (val) => val && val.length
+const minLength = (len) => (val) => !val || val.length >= len
+const maxLength = (len) => (val) => !val || val.length <= len
+
+class CommentForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(values) {
+        this.toggleModal();
+        alert("Rating: " + values.rating + " Name: " + values.author
+            + " Comment: " + values.comment);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleLogin(values)} >
+                            <Row className="form-group">
+                                <Label md={12} htmlFor="rating">Rating</Label>
+                                <Col>
+                                    <Control.select model=".rating" id="rating" name="rating" className="form-control " defaultValue="1">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label md={12} htmlFor="author">Your Name</Label>
+                                <Col>
+                                    <Control.text model=".author" id="author" name="author" className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }} />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label md={12} htmlFor="comment">Comment</Label>
+                                <Col>
+                                    <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="form-control"
+                                    />
+                                </Col>
+                            </Row>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil"></span> Submit Comment</Button>
+            </React.Fragment>
+        );
     }
 }
 
