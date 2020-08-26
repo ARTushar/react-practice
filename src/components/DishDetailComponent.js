d4e31cc97d6e2f6b64b0d283ce4cc695538c5c08
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-08-26 23:08:06 +0600

    redux thunk and logger

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent'
 
 
 function RenderDish({ dish }) {
@@ -54,7 +55,23 @@ function RenderComments({ comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null) {
+    if(props.isLoading){
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    } else if(props.errMess){
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    } else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
